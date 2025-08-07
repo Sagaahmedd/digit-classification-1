9aec9df4dcb16bd7ebbf44eabfd5a6c7d328fc1b
Asser Ahmed <asserahmed08@gmail.com>
2025-08-07 15:33:45 +0300

    created the gradio app

--- app.py
+++ app.py
@@ -1,38 +1,46 @@
-from tensorflow import keras
-from PIL import Image
-import numpy as np
 import gradio as gr
-import warnings
-warnings.filterwarnings('ignore')
+import numpy as np
+import tensorflow as tf
+from PIL import Image
 
-model = keras.models.load_model("model.keras")
+model = tf.keras.models.load_model("model.keras")
+with open("styling.css") as f:
+    css = f.read()
 
 def predict(sketchpad_input):
-    if isinstance(sketchpad_input,dict):
-        img=sketchpad_input["composite"]
+    if isinstance(sketchpad_input, dict):
+        img = sketchpad_input["composite"]
     else:
-        img=sketchpad_input
+        img = sketchpad_input
+    if isinstance(img, np.ndarray):
+        img = Image.fromarray(img)
 
-    if isinstance(img,np.ndarray):
-        img=Image.fromarray(img)
+    img = img.convert("L").resize((28, 28))
 
-    img=img.convert("L")
-    img=img.resize((28,28))
-
-    img_array=np.array(img)
-    img_array=img_array / 255.0
-    img_array=img_array.reshape(1,784)
+    img_array = np.array(img)
+    img_array = img_array / 255.0
+    img_array = img_array.reshape(1, 784)
     img_array = 1.0 - img_array
 
     probabilities = model.predict(img_array)[0]
-    output = ""
-    for i, p in enumerate(probabilities):
-        output += f"{i}: {p*100:.2f}% \n"
-    output += f"\nIt is (most likely): {np.argmax(probabilities)}"
-    return output
+    probs = {str(i): float(probabilities[i]) for i in range(10)}
+    return probs
 
-ui=gr.Interface(fn=predict,inputs=gr.Sketchpad(),outputs="text")
+def clear():
+    return None, {}
 
-ui.launch()
+with gr.Blocks(css=css) as demo:
+    gr.Markdown("## What digit is it?", elem_classes=["title"])
+    with gr.Row():
+        with gr.Column():
+            sketchpad = gr.Sketchpad()
+            predict_btn = gr.Button("Predict")
+            clear_btn = gr.Button("Clear")
+        
+        with gr.Column():
+            label_output = gr.Label(num_top_classes=10, label="Prediction")
+    predict_btn.click(fn=predict, inputs=sketchpad, outputs=label_output)
+    clear_btn.click(fn=lambda: None, inputs=None, outputs=[sketchpad, label_output])
 
+demo.launch()
 
